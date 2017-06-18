e1d7df4e0b38c89e24b50f147554a79fbe4c4c9d
mahmoudeid <aze3ma@gmail.com>
2017-06-18 15:48:09 +0200

    migrate database

--- db/migrate/20170618134356_create_conversation_members.rb
+++ db/migrate/20170618134356_create_conversation_members.rb
@@ -2,7 +2,7 @@ class CreateConversationMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :conversation_members do |t|
       t.references :conversation_id, foreign_key: true
-      t.reference :user_id
+      t.references :user_id
       t.timestamp :seen_at
 
       t.timestamps
