84e6c436750cf89205674d572a8406b7e4892e7d
kamalhosny <kamal-aldin-hosny@hotmail.com>
2017-06-18 14:29:21 +0200

    finish user model and controller and the controller tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe 'GET #index' do
+    it 'returns a success response' do
+      user = User.create! :valid_user
+      get :index, params: {}
+      expect(response).to be_success
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns a success response' do
+      user = User.create! :valid_user
+      get :show, params: { id: user.to_param }, format: :json
+      expect(response).to be_success
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new User' do
+        expect do
+          post :create, params: { user: :valid_user }
+        end.to change(User, :count).by(1)
+      end
+
+      it 'renders a JSON response with the new user' do
+        post :create, params: { user: :valid_user }
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to eq('application/json')
+        expect(response.location).to eq(user_url(User.last))
+      end
+    end
+
+    context 'with invalid params' do
+      it 'renders a JSON response with errors for the new user' do
+        post :create, params: { user: :invalid_user_1 }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    context 'with valid params' do
+      it 'updates the requested user' do
+        user = User.create! :valid_user
+        put :update, params: { id: user.to_param, user: { first_name: 'kamal' } }
+        user.reload
+      end
+
+      it 'renders a JSON response with the user' do
+        user = User.create! :valid_user
+
+        put :update, params: { id: user.to_param, user: :valid_user }
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+
+    context 'with invalid params' do
+      it 'renders a JSON response with errors for the user' do
+        user = User.create! :valid_user
+
+        put :update, params: { id: user.to_param, user: :invalid_user }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'destroys the requested user' do
+      user = User.create! valid_attributes
+      expect do
+        delete :destroy, params: { id: user.to_param }
+      end.to change(User, :count).by(-1)
+    end
+  end
+end
