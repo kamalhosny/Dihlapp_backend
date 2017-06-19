8618c0d330d8150147e993cae8a59d9e7fc71b8b
Pakinam <YOUR@EMAIL.com>
2017-06-19 13:00:21 +0200

    modify conversation controller

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,13 +1,15 @@
 class Conversation < ApplicationRecord
   # belongs_to :sender, class_name: 'User'
   # belongs_to :receiver, class_name: 'User'
+
+  ## Associations
   has_many :users, through: :conversation_members
   has_many :messages, -> { order(created_at: :asc) }, dependent: :destroy
 
   #
-  # scope :participating, -> (user) do
-  #   where("(conversations.sender_id = ? OR conversations.receiver_id = ?)", user.id, user.id)
-  # end
+  scope :participating, -> (user) do
+    where("(conversations.conversation_members.user_id = ?)", user.id)
+  end
   #
   # scope :between, -> (sender_id, receiver_id) do
   # where(sender_id: sender_id, receiver_id: receiver_id).or(where(sender_id: receiver_id, receiver_id: sender_id)).limit(1)
