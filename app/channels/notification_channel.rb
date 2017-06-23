5b900474de151d08f168fc8c2880f5f1d4d5822f
Pakinam <YOUR@EMAIL.com>
2017-06-23 19:15:30 +0200

    add comments

--- app/channels/notification_channel.rb
+++
@@ -1,7 +0,0 @@
-# class NotificationChannel < ApplicationCable::Channel
-#   def subscribed
-#   	stream_from "message"
-#   end
-# end
-
-# No need for this channel as we are using the chat_channel and can comment or delete this whole file
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   before_action :current_user
   def current_user
+    # a patch until the current user issue is fixed
     @current_user=User.first
     # @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
